87de8cad8ce0f446f0155ca43d1ca9ee6e841f41
unknown <gabriel.da.silva.marques.15@gmail.com>
2019-04-14 21:14:27 +0200

    add swippable menu and translation (nothing work)

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,18 +6,38 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Translater } from './translater';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    BrowserModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (createTranslateLoader),
+        deps: [HttpClient]
+    }})],
   providers: [
     StatusBar,
     SplashScreen,
+    Translater,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
--- client/src/app/tab3/tab3.page.ts
+++ client/src/app/tab3/tab3.page.ts
@@ -1,8 +1,28 @@
 import { Component } from '@angular/core';
+import { Translater } from '../translater';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {}
+export class Tab3Page {
+
+	/** Translater object, is not used yet because there is no text */
+  private translate: Translater;
+  /** Will contain all available languages for user selection */ 				
+	private currentLangs: any;
+
+  constructor(translate: Translater) {
+
+		this.translate = translate;
+    this.currentLangs = this.translate.getAllLanguages();
+    this.translate.changeLanguage('fr');
+  }
+  
+  unread(item){
+    console.log("coucou");
+  }
+
+
+}
