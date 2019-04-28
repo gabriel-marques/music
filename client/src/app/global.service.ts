fbc6fd5af5a204cfeade74d86755297145e9ba97
unknown <gabriel.da.silva.marques.15@gmail.com>
2019-04-28 21:03:28 +0200

    add global array as service

---
+++ client/src/app/global.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GlobalService {
+
+  tracks = [];
+
+  constructor() { }
+
+  addTrack(track){
+    this.tracks.unshift(track);
+    this.tracks = [...this.tracks]; // force refresh of view
+  }
+
+  removeTrack(track){
+    // get index of track in global array
+    var index = this.tracks.findIndex(t => t.track == track);
+    this.tracks.splice(index,1); // delete the track
+    this.tracks = [...this.tracks]; // force refresh of view
+  }
+
+  updateVote(track){
+    // get index of track in global array
+    var index = this.tracks.findIndex(t => t.track == track['track']);
+    // set the value of the track in the array
+    this.tracks[index].votes = track['votes'];
+    
+    this.tracks = [...this.tracks]; // force refresh of view
+  }
+
+}
