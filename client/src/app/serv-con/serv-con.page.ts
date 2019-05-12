9b2065c536c34b0e906e802de56ba61aa58aca29
Unknown <buffopi@gmail.com>
2019-05-12 16:15:39 +0200

    Working on loading page

--- client/src/app/serv-con/serv-con.page.ts
+++ client/src/app/serv-con/serv-con.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-serv-con',
@@ -7,12 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServConPage implements OnInit {
 
-  constructor() { }
+  constructor(public loadingController: LoadingController) {
+    this.presentLoading()
+  }
+
+  async presentLoading() {
+    const loading = await this.loadingController.create({
+      message: 'Looking up for your friends, please wait for a second...',
+      duration: 6000
+    });
+    await loading.present();
+
+    const { role, data } = await loading.onDidDismiss();
+
+    console.log('Loading dismissed!');
+    this.presentLoadingWithOptions()
+  }
+
+  async presentLoadingWithOptions() {
+    const loading = await this.loadingController.create({
+      spinner: null,
+      duration: 2000,
+      message: 'Connecting to xx\'s servers',
+      translucent: true,
+      cssClass: 'custom-class custom-loading'
+    });
+    return await loading.present();
+  }
 
   ngOnInit() {
   }
 
-  coucou(){
-    console.log("BIIIIIITTTTTEEEEE");
-  }
 }
